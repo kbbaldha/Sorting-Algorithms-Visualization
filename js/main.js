d19309780968bc896a45a028b5ea823fbb0deb85
kbbaldha <keyurbaldha@gmail.com>
2018-01-14 15:25:21 -0500

    Added initial sorting functionality

--- js/main.js
+++ js/main.js
@@ -21,27 +21,7 @@ function shuffleArray(array) {
         array[j] = temp;
     }
 }
-var colors = [[255,0,0],[0,255,0],[0,0,255],[255,255,0],[0,255,255],[255,100,255],
-[100,200,200],[135,44,36],[44,55,66],[200,235,222],[122,33,49],[255,220,222]];
-var $tmparr = [];
-for(var i=0;i<w;i++){
 
-    //$tmparr.push([Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256)]);
-    $tmparr.push(colors[Math.floor(Math.random()*colors.length)]);
-}
-
-
-
-for(var i=0;i<h;i++){
-    shuffleArray($tmparr);
-    for(var j=0;j<w*4;j+=4){
-        var base = i*w*4;
-        imgData.data[base+j+0] = $tmparr[j/4][0];
-        imgData.data[base+j+1] = $tmparr[j/4][1];
-        imgData.data[base+j+2] = $tmparr[j/4][2];
-        imgData.data[base+j+3] = 255;
-    }
-}
 /*for (var i = 0; i < imgData.data.length; i += 4) {
     imgData.data[i+0] = Math.floor(Math.random() * 255);
     imgData.data[i+1] = Math.floor(Math.random() * 255);
@@ -49,7 +29,7 @@ for(var i=0;i<h;i++){
     imgData.data[i+3] = 255;
 }*/
 
-ctx.putImageData(imgData, 0, 0);
+
 //bubblesort(imgData,ctx);
 function compare(data,left,right){
     var sumLeft = 0, sumRight = 0;
@@ -67,3 +47,47 @@ function swap(data,left,right){
         data[right+i] = tmp;
     }
 }
+
+
+
+
+
+
+function reset(shuffleNotReqd,isAscending){
+
+    if(shuffleNotReqd){
+        //$tmparr = sortColors($tmparr,isAscending);
+        if(isAscending){
+            $tmparr.sort(function(a,b){ return getVal(a,0)-getVal(b,0); });
+        }
+        else{
+            $tmparr.sort(function(a,b){ return getVal(b,0)-getVal(a,0); });
+        }
+    }
+    console.log("reset");
+    for(var i=0;i<h;i++){
+        if(shuffleNotReqd != true)
+            shuffleArray($tmparr);
+        for(var j=0;j<w*4;j+=4){
+            var base = i*w*4;
+            imgData.data[base+j+0] = $tmparr[j/4][0];
+            imgData.data[base+j+1] = $tmparr[j/4][1];
+            imgData.data[base+j+2] = $tmparr[j/4][2];
+            imgData.data[base+j+3] = 255;
+        }
+    }
+    ctx.putImageData(imgData, 0, 0);
+}
+
+var colors = [[255,0,0,255],[0,255,0,255],[0,0,255,255],[255,255,0,255],[0,255,255,255],[255,100,255,255],
+[100,200,200,255],[135,44,36,255],[44,55,66,255],[200,235,222,255],[122,33,49,255],[255,220,222,255]];
+var $tmparr = [];
+for(var i=0;i<w;i++){
+
+    //$tmparr.push([Math.floor(Math.random() * 256),Math.floor(Math.random() * 256),Math.floor(Math.random() * 256)]);
+    $tmparr.push(colors[Math.floor(Math.random()*colors.length)]);
+}
+
+
+
+reset();
